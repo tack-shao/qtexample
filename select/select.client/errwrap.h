b3b6c175998c8a9c4e1f5c3f90fdba76b34495ab
weiwei <wxjlmr@126.com>
2018-06-13 07:31:57 +0800

    add select example

---
+++ select/select.client/errwrap.h
@@ -0,0 +1,15 @@
+#define Socket     socket    
+#define Bind       bind      
+#define Listen     listen    
+#define perr_exit  perror 
+#define Accept     accept    
+#define Read       read      
+#define Close      close     
+#define Write      write     
+
+
+
+#define Connect    connect
+#define err_exit   perror 
+
+
---
+++ select/select.server/errwrap.h
@@ -0,0 +1,10 @@
+#define Socket     socket    
+#define Bind       bind      
+#define Listen     listen    
+#define perr_exit  perror 
+#define Accept     accept    
+#define Read       read      
+#define Close      close     
+#define Write      write     
+
+
