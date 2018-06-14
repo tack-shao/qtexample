faa8c1d5f171a7b77fbab26c06c0f9ec68921e2c
weiwei <wxjlmr@126.com>
2018-06-14 23:40:29 +0800

    使用#ifndef PUBNOTE("xxx") #endif 添加注释，区分类别

---
+++ pubnote/main.cpp
@@ -0,0 +1,19 @@
+
+
+#ifndef PUBNOTE ("头文件")
+#include <iostream>
+
+using namespace std;
+
+#endif /*头文件*/
+
+
+#ifndef PUBNOTE ("函数或类实现")
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+#endif /*函数或类实现*/
+
